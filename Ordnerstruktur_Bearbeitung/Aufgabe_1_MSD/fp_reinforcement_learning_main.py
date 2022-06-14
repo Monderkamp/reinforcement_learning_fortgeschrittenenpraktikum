256723c19a54b6fba9e090aaa2979cbc37e76845
Monderkamp <44001253+Monderkamp@users.noreply.github.com>
2022-06-14 17:06:09 +0200

    restored gif and folders

--- Ordnerstruktur_Bearbeitung/Aufgabe_1_MSD/fp_reinforcement_learning_main.py
+++ Ordnerstruktur_Bearbeitung/Aufgabe_1_MSD/fp_reinforcement_learning_main.py
@@ -1,8 +1,10 @@
 import fp_classes
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.offsetbox import TextArea, DrawingArea, OffsetImage, AnnotationBbox
 import time as tm
 
+from celluloid import Camera
 from datetime import date
 import datetime
 
@@ -56,7 +58,7 @@ plt.show()
 
 
 exit()
-
+LAST_TRAJECTORY = []
 
 
 for episode in range(learner.N_episodes):
@@ -69,6 +71,49 @@ for episode in range(learner.N_episodes):
 	learner.chosen_action = None
 
 	while (learner.x != env.target_position) or learner.chosen_action != 1:
+		if episode == learner.N_episodes-1: LAST_TRAJECTORY.append(learner.x)
 		pass
 
+
+im_agent = plt.imread("../IMG/Lasse_head.png")
+im_treat = plt.imread("../IMG/treat.png")
+imagebox = OffsetImage(im_agent, zoom=0.02)
+imagebox2 = OffsetImage(im_treat, zoom=0.5)
+
+trajfig,trajax = plt.subplots()
+camera = Camera(trajfig)
+
+#--------------------------------------------------------------------
+#IM FOLGENDEN CODE BLOCK WIRD DIE TRAJEKTORIE ALS GIF AUSGEGEBEN
+#WENN DIESER TEIL ZU LANGE DAUERT ODER SIE DIE NÖTIGEN LIBRARIES NICHT INSTALLIEREN KÖNNEN,
+#KOMMENTIEREN SIE DIESEN CODE BLOCK AUS
+#--------------------------------------------------------------------
+
+
+LAST_TRAJECTORY_WITHOUT_PBJUMPS = []
+for i in range(len(LAST_TRAJECTORY)-1):
+	dx = np.abs(LAST_TRAJECTORY[i+1] - LAST_TRAJECTORY[i])
+	LAST_TRAJECTORY_WITHOUT_PBJUMPS.append(LAST_TRAJECTORY[i])
+	if dx > 0.5*env.N_states:
+		LAST_TRAJECTORY_WITHOUT_PBJUMPS.append(None)
+		
+
+for i,x in enumerate(LAST_TRAJECTORY_WITHOUT_PBJUMPS):
+	trajax.plot(np.array(LAST_TRAJECTORY_WITHOUT_PBJUMPS)[:i],np.linspace(0,0,len(np.array(LAST_TRAJECTORY_WITHOUT_PBJUMPS)[:i])),color="red")	
+	ab = AnnotationBbox(imagebox2, (env.target_position, 0.0),frameon=False)
+	trajax.add_artist(ab)
+	if x:
+		ab = AnnotationBbox(imagebox, (x, 0.0),frameon=False)
+		#trajax.add_artist(ab)
+	else:
+		ab = AnnotationBbox(imagebox, (LAST_TRAJECTORY_WITHOUT_PBJUMPS[i+1], 0.0),frameon=False)
+	trajax.add_artist(ab)
+			
+
+	trajax.set_xlim(0.0,env.N_states)
+	trajax.set_ylim(-1.0,1.0)
+	trajax.set_yticks([])
+	camera.snap()
+animation = camera.animate()		
+animation.save("LAST_TRAJECTORY_" + now + ".gif")
 
